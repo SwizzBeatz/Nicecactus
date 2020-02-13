d2329c7850aace1cf5324dd97f648d0315539f55
SwizzBeatz <notifications.idevice@gmail.com>
2020-02-13 08:14:45 +0100

    Update Nicecactus.js

--- Nicecactus.js
+++ Nicecactus.js
@@ -1,4 +1,4 @@
-const axios = require ('axios');
+import { post, get } from 'axios';
 const login_url = 'https://api.nicecact.us/auth/login';
 const login_email = 'contact@nba2kcontest.com'
 const login_password = 'Atsiv09!'
@@ -9,13 +9,13 @@ var axios_headers = {
            'Pragma': 'no-cache',}
 
 const tournament_list_url = 'https://api.nicecact.us/matchmaking/tournament?populate_game=true&limit=10&page=1&sort_startDate=desc&no_description=true&no_rules=true&no_prizesInformation=true&$where=function()%20%7Bfor%20(let%20key%20in%20this.name)%20%7Bif%20(/proam/i.test(this.name%5Bkey%5D))%20%7Breturn%20true;%7D%7Dreturn%20false;%7D&game=5d765ff523ef1b11c02ceb07'
-
 // fonction principale 
 async function main() {
 
+// fonction login pour récupérer l'access token
   login = async () => {
     try {
-      const response  = await axios.post(
+      const response  = await post(
         login_url, 
         {email:login_email,password:login_password},
         {headers:axios_headers
@@ -29,43 +29,67 @@ async function main() {
   const accessToken = await login();
 //console.log(accessToken)
 
-// récupération de la liste des tournois
+// récupération  de la liste des tournois avec le terme "proam"
 Tournament_List = async (token) => {
     try {
-      const response = await axios.get(
-        tournament_list_url, 
-        {headers:{
-           'x-access-token': token
-          }})
-        
-          console.log(response.data.data.docs[0].name)
-          console.log(response.data.data.docs[0].id)
-          return response.data;
-        // console.log(response.data.data[0].members)
-  } catch (error) {
+      const tournament_list_response = await get(tournament_list_url, {headers:{'x-access-token': token}})
+      let tournamentlist = tournament_list_response.data.data.docs;
+      let tournaments_list_results =[];
+      let participant_list_results =[];
+      // console.log(tournament_list_response.data.data.docs)
+      for (var tournament of tournamentlist) {     
+        const tournament_id = tournament.id;
+        const tournament_name = tournament.name;
+        const participants_list_url= 'https://api.nicecact.us/matchmaking/tournament/'+tournament_id+'/participants';
+        //console.log (participants_list_url);
+        // console.log (tournament_id);
+        //console.log (tournament_name);
+        let tournament_info = {
+          TournamentID: tournament_id,
+          TournamentName: tournament_name,
+          url: participants_list_url
+        }
+        let ParticipantsUrl = {
+          url: participants_list_url
+        } 
+        participant_list_results.push(ParticipantsUrl);
+        tournaments_list_results.push(tournament_info);
+        // console.log (participants_list_response.data.data[0].name);
+      }
+      console.log(tournaments_list_results);
+      return tournaments_list_results;
+        // return response.data.data;
+        } catch (error) {
     console.log(error.message)
   }
   }
-Tournament_List(accessToken);
+await Tournament_List(accessToken);
 
-//récupération de l'id d'une team
-Team_id = async (token) => {
+
+// récupération  de la liste des participants des tournois 
+Participant_List = async (url, token) => {
   try {
-    const response = await axios.get(
-      'https://api.nicecact.us/api/team?like_name=bayern&populate_game=true', 
-      {headers:{
-         'x-access-token': token
-        }})
-      
-        console.log(response.data.data[0]._id)
-        return response.data.data[0]._id;
-      // console.log(response.data.data[0].members)
-} catch (error) {
+    const participants_list_response = await get(url, {headers:{'x-access-token': token}})
+    let participants_list = participants_list_response.data.data;
+    //console.log (participants_list);
+    let participant_list_results =[];
+    // console.log(tournament_list_response.data.data.docs)
+    for (var participant of participants_list) {     
+      const participant_id = participant.id;
+      const participant_name = participant.name;
+      console.log (participant_id);
+      console.log (participant_name);
+      // console.log (participants_list_response.data.data[0].name);
+    }
+      // return response.data.data;
+      } catch (error) {
   console.log(error.message)
-}
+  }
+  }
+  await Participant_List('https://api.nicecact.us/matchmaking/tournament/5e386b8e49b3c79ecf15a32c/participants',accessToken);
 }
 
-//Team_id (accessToken);
-}
+
+
 
 main ();
